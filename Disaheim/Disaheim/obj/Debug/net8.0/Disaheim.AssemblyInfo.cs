1c04cd19efbe7e857e8f20cafa5646b5e336c5a8
Martin Jul <martin.henriksen@3mj.dk>
2025-10-08 10:04:13 +0200

    Git changes

--- Disaheim/Disaheim/obj/Debug/net8.0/Disaheim.AssemblyInfo.cs
+++ Disaheim/Disaheim/obj/Debug/net8.0/Disaheim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Disaheim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069b18aae30132f82db05aa6974b8ed3d1cac96e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f513bfc697f37b0593b8355c4b7e4667779b32aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Disaheim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Disaheim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

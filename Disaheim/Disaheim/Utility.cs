5e836d6b0c2d266e2aff333bb29f21941e55b4a2
Martin Jul <martin.henriksen@3mj.dk>
2025-10-09 09:17:12 +0200

    Metoder tilføjet

---
+++ Disaheim/Disaheim/Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    internal class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            double price = 0;
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    price = 12.5;
+                    break;
+                case Level.medium:
+                    price = 20;
+                    break;
+                case Level.high:
+                    price = 27.5;
+                    break;
+            }
+            return price;
+        }
+    }
+}
--- Disaheim/Disaheim/obj/Debug/net8.0/Disaheim.AssemblyInfo.cs
+++ Disaheim/Disaheim/obj/Debug/net8.0/Disaheim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Disaheim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895bd3de8364374af9c47d34a2b2d6d634221d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314a7fd6226bcb87616578e06279befe27e00785")]
 [assembly: System.Reflection.AssemblyProductAttribute("Disaheim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Disaheim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
